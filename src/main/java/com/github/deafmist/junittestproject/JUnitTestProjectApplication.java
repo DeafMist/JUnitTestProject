6270ee9ec20b2fbdb8ebe93c1ba404340068fadf
DeafMist <romapr70@gmail.com>
2023-02-01 21:28:45 +0300

    Initial commit

---
+++ src/main/java/com/github/deafmist/junittestproject/JUnitTestProjectApplication.java
@@ -0,0 +1,13 @@
+package com.github.deafmist.junittestproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JUnitTestProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JUnitTestProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/deafmist/junittestproject/JUnitTestProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.deafmist.junittestproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JUnitTestProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
